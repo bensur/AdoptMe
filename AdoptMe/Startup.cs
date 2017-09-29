95132f9a718851b3ee745b35afb39b91fb3ac9b9
ben <ben@DESKTOP-NBE7N40>
2017-09-29 19:29:17 +0300

    Add project files.

---
+++ AdoptMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdoptMe.Startup))]
+namespace AdoptMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

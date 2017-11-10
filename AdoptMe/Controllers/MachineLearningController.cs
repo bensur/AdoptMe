08a5e54b9831ce7847469b86b24dbedf79f12000
Ben Surkiss <bensur8@gmail.com>
2017-11-10 11:37:52 +0200

    Add ML

--- AdoptMe/Controllers/MachineLearningController.cs
+++ AdoptMe/Controllers/MachineLearningController.cs
@@ -26,32 +26,32 @@ namespace AdoptMe.Controllers
            Location (Darom=0,Merkaz=1,Tzafon=2),
            Salary (<5000=0,5000<<10000=1,10000<=2)
         */
-        public ActionResult Decide(string gender, int age, string location, int salary)
+        public ActionResult Decide(string gender, double age, string location, double salary)
         {
-            if (gender.Equals("Male"))
-                gender = "0";
+            if ((gender != null) && gender.Equals("Male"))
+                gender = "0.0";
             else
-                gender = "1";
-            if (age < 18)
-                age = 0;
-            else if (age < 25)
-                age = 1;
-            else if (age < 40)
-                age = 2;
+                gender = "1.0";
+            if (age < 18.0)
+                age = 0.0;
+            else if (age < 25.0)
+                age = 1.0;
+            else if (age < 40.0)
+                age = 2.0;
             else
-                age = 3;
-            if (location.Equals("Darom"))
-                location = "0";
-            else if (location.Equals("Merkaz"))
-                location = "1";
+                age = 3.0;
+            if ((location != null) && location.Equals("Darom"))
+                location = "0.0";
+            else if ((location != null) && location.Equals("Merkaz"))
+                location = "1.0";
             else
-                location = "2";
-            if (salary < 5000)
-                salary = 0;
-            else if (salary < 10000)
-                salary = 1;
+                location = "2.0";
+            if (salary < 5000.0)
+                salary = 0.0;
+            else if (salary < 10000.0)
+                salary = 1.0;
             else
-                salary = 2;
+                salary = 2.0;
             return Content(ml.Decide(new double[] { Double.Parse(gender), (double)age, Double.Parse(location), (double)salary }));
         }
     }
